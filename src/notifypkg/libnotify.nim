340dc9d02e6234394b8829f315bb12ab4296fe5b
Xabi Bello <xbello@gmail.com>
2019-03-22 12:33:49 +0100

    Added the lib and the proc wrapper.

--- src/notifypkg/libnotify.nim
+++ src/notifypkg/libnotify.nim
@@ -6,12 +6,13 @@ type
   GError = object
   GErrorPtr* = ptr GError
 
-{.push dynlib: "libnotify.so".}
+{.push dynlib: "libnotify.so.4".}
 proc notify_init*(s: cstring): bool {.importc, discardable.}
 proc notify_uninit*() {.importc.}
 
 proc notify_notification_new*(sumary, body, icon: cstring): NotifyNotificationPtr {.importc.}
 proc notify_notification_show*(notification: NotifyNotificationPtr, error: GErrorPtr): bool {.importc.}
 proc notify_notification_update*(notification: NotifyNotificationPtr, sumary, body, icon: cstring): bool {.importc.}
+proc notify_notification_set_timeout*(notification: NotifyNotificationPtr, timeout: cint): void {.importc.}
 
 {.pop.}
